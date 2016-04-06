109f20d6e9c76dcdc90df67a125f53c7f0c3ab94
Cody Smith <smartkid101@gmail.com>
2016-04-06 16:37:26 -0600

    Working on donation

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -744,7 +744,7 @@ void recalculate_priority(struct thread * foo) {
       // Check donated priorities
       for(i = 0; i < 8; ++i) {
 
-        if(bar->priDon[i].thread == foo {
+        if(bar->priDon[i].thread == foo) {
 
           printf("Altering donated priority of %s from %d to %d\r\n", bar->name, bar->priDon[i].priority, foo->priority);
 
