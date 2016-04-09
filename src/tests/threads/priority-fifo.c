17cee018f066c30bfb31e9af120ad4e4698b0ea6
Cody Smith <smartkid101@gmail.com>
2016-04-09 15:55:57 -0600

    Working on sema

--- src/tests/threads/priority-fifo.c
+++ src/tests/threads/priority-fifo.c
@@ -15,128 +15,85 @@
 #include "threads/synch.h"
 #include "threads/thread.h"
 
-struct simple_thread_data {
-  int id;                     /* Sleeper ID. */
-  int iterations;             /* Iterations so far. */
-  struct lock *lock;          /* Lock on output. */
-  int **op;                   /* Output buffer position. */
-};
+struct simple_thread_data 
+  {
+    int id;                     /* Sleeper ID. */
+    int iterations;             /* Iterations so far. */
+    struct lock *lock;          /* Lock on output. */
+    int **op;                   /* Output buffer position. */
+  };
 
 #define THREAD_CNT 16
 #define ITER_CNT 16
 
 static thread_func simple_thread_func;
 
-void test_priority_fifo(void) {
-
+void
+test_priority_fifo (void) 
+{
   struct simple_thread_data data[THREAD_CNT];
   struct lock lock;
   int *output, *op;
   int i, cnt;
 
   /* This test does not work with the MLFQS. */
-  ASSERT(!thread_mlfqs);
+  ASSERT (!thread_mlfqs);
 
   /* Make sure our priority is the default. */
-  ASSERT(thread_get_priority() == PRI_DEFAULT);
+  ASSERT (thread_get_priority () == PRI_DEFAULT);
 
-  msg("%d threads will iterate %d times in the same order each time.", THREAD_CNT, ITER_CNT);
-  
-  msg("If the order varies then there is a bug.");
+  msg ("%d threads will iterate %d times in the same order each time.",
+       THREAD_CNT, ITER_CNT);
+  msg ("If the order varies then there is a bug.");
 
-  output = op = malloc(sizeof *output * THREAD_CNT * ITER_CNT * 2);
+  output = op = malloc (sizeof *output * THREAD_CNT * ITER_CNT * 2);
+  ASSERT (output != NULL);
+  lock_init (&lock);
 
-  ASSERT(output != NULL);
+  thread_set_priority (PRI_DEFAULT + 2);
+  for (i = 0; i < THREAD_CNT; i++) 
+    {
+      char name[16];
+      struct simple_thread_data *d = data + i;
+      snprintf (name, sizeof name, "%d", i);
+      d->id = i;
+      d->iterations = 0;
+      d->lock = &lock;
+      d->op = &op;
+      thread_create (name, PRI_DEFAULT + 1, simple_thread_func, d);
+    }
 
-  lock_init(&lock);
-
-  thread_set_priority(PRI_DEFAULT + 2);
-
-  for(i = 0; i < THREAD_CNT; i++) {
-
-    char name[16];
-
-    struct simple_thread_data *d = data + i;
-
-    snprintf(name, sizeof name, "%d", i);
-
-    d->id = i;
-    d->iterations = 0;
-    d->lock = &lock;
-    d->op = &op;
-
-    // Added
-    printf("Creating thread... %s", name);
-
-    thread_create(name, PRI_DEFAULT + 1, simple_thread_func, d);
-
-    // Added
-    printf(" complete.\r\n");
-
-  }
-
-  // Added
-  printf("Thread %s: Changing priority...\r\n", thread_current()->name);
-  thread_set_priority(PRI_DEFAULT);
-  // Added
-  printf("Thread %s: Priority changed.\r\n", thread_current()->name);
-
+  thread_set_priority (PRI_DEFAULT);
   /* All the other threads now run to termination here. */
-  ASSERT(lock.holder == NULL);
+  ASSERT (lock.holder == NULL);
 
   cnt = 0;
-  for(; output < op; output++) {
+  for (; output < op; output++) 
+    {
+      struct simple_thread_data *d;
 
-    struct simple_thread_data *d;
-
-    ASSERT(*output >= 0 && *output < THREAD_CNT);
-
-    d = data + *output;
-
-    if (cnt % THREAD_CNT == 0) {
-      printf("(priority-fifo) iteration:");
+      ASSERT (*output >= 0 && *output < THREAD_CNT);
+      d = data + *output;
+      if (cnt % THREAD_CNT == 0)
+        printf ("(priority-fifo) iteration:");
+      printf (" %d", d->id);
+      if (++cnt % THREAD_CNT == 0)
+        printf ("\n");
+      d->iterations++;
     }
-
-    printf(" %d", d->id);
-
-    if(++cnt % THREAD_CNT == 0) {
-      printf("\n");
-    }
-
-    d->iterations++;
-
-  }
-
 }
 
-static void simple_thread_func (void *data_) {
-
-  // Added
-  printf("Thread %s: Started.\r\n", thread_current()->name);
-
+static void 
+simple_thread_func (void *data_) 
+{
   struct simple_thread_data *data = data_;
   int i;
   
-  for(i = 0; i < ITER_CNT; i++) {
-
-    // Added
-    printf("Thread %s: Acquiring...\r\n", thread_current()->name);
-    lock_acquire(data->lock);
-
-    // Added
-    printf("Thread %s: Acquired.\r\n", thread_current()->name);
-    *(*data->op)++ = data->id;
-
-    // Added
-    printf("Thread %s: Releasing...\r\n", thread_current()->name);
-    lock_release(data->lock);
-    // Added
-    printf("Thread %s: Released.\r\n", thread_current()->name);
-
-    thread_yield();
-
-  }
-
+  for (i = 0; i < ITER_CNT; i++) 
+    {
+      lock_acquire (data->lock);
+      *(*data->op)++ = data->id;
+      lock_release (data->lock);
+      thread_yield ();
+    }
 }
-
-
